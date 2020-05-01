684100161b7de32e2d816914a44f6075e4ca3b70
est271 <56150991+est271@users.noreply.github.com>
2020-05-01 12:16:37 -0700

    Revise nav bars and other stuff

--- scripts/main.js
+++ scripts/main.js
@@ -6,4 +6,3 @@
 
 $(".top-nav-bar").load("../templates/top.html");
 $(".bot-nav-bar-1").load("../templates/bot.html nav");
-$(".bot-nav-bar-2").load("../templates/bot.html nav").addClass("fixed-bottom");
