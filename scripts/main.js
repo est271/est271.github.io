c5c01d18833a7624839ecffac09d376df06f0894
est271 <56150991+est271@users.noreply.github.com>
2020-05-01 19:46:45 -0700

    added fade in effect to page load

--- scripts/main.js
+++ scripts/main.js
@@ -7,10 +7,14 @@
 $(".top-nav-bar").load("../templates/top.html");
 $(".bot-nav-bar-1").load("../templates/bot.html nav");
 
+$(document).ready( (event) => {
+    $("body").hide(0).delay(100).fadeIn(400);
+});
+
 $(document).on("click", ".nav-link, .btn", (event) => {
     let href = event.target.href;
     event.preventDefault();
-    $(".jumbotron, .container").fadeOut(700, () => {
+    $("body").fadeOut(700, () => {
         window.location = href;
     });
 });
