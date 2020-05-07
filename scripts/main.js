e382214d23972ea9fd6cbfef1ab5aa450a7d3e2d
est271 <56150991+est271@users.noreply.github.com>
2020-05-06 17:12:14 -0700

    added portfolio to projects & added skills used section

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,22 @@
 
 "use strict";
 
+// nav-bar templates
 $(".top-nav-bar").load("../templates/top.html");
-$(".bot-nav-bar-1").load("../templates/bot.html nav");
+$(".bot-nav-bar-1").load("../templates/bot.html");
+$(".bot-nav-bar-2").load("../templates/bot.html").addClass("fixed-bottom");
+
+// skill button templates
+$("#project-1-html").load("../templates/skill_btn.html #project-1-btn");
+$("#project-2-html").load("../templates/skill_btn.html #project-2-btn");
+$("#project-3-html").load("../templates/skill_btn.html #project-3-btn");
+$("#project-4-html").load("../templates/skill_btn.html #project-4-btn");
 
 $(document).ready( () => {
     $(".fade1").fadeTo(700, 1.0);
 });
 
-$(document).on("click", ".nav-link, .btn", (event) => {
+$(document).on("click", ".nav-link, .btn-primary", (event) => {
     let href = event.target.href;
     event.preventDefault();
     $(".fade1").fadeOut(700, () => {
